61ffb37654b81d9a8207890b21b0d63510fcd11b
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2024-05-17 12:17:06 -1200

    ruta produccion

--- src/routes/Produccion.routes.js
+++ src/routes/Produccion.routes.js
@@ -10,7 +10,7 @@ const rutaProduccion = Router()
 rutaProduccion.get('/listarProduccion',validarToken,listarProduccion);
 rutaProduccion.post('/RegistraProduccion',validarToken, ValidateProduccion,registrarProduccion);
 rutaProduccion.get('/BuscarProduccion/:id_producccion', validarToken, BuscarProduccion);
-rutaProduccion.put('/desactivarProduccion/:id_produccion', validarToken, desactivarProduccion);
+rutaProduccion.put('/desactivarProduccion/:id_produccion', validarToken, DesactivarProduccion);
 rutaProduccion.put('/ActualizarProduccion/:id_producccion',validarToken,actualizar,actualizarProduccion);
 rutaProduccion.put('/DesactivarProduccion/:id',validarToken,DesactivarProduccion);
 export default rutaProduccion;
